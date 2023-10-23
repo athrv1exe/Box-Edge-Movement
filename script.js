be2b2c38c7aca0db0f857159e183ab5dfc767628
atharva <atharvamk6@gmail.com>
2023-10-23 22:31:17 +0530

    final touch

--- script.js
+++ script.js
@@ -16,20 +16,28 @@ var direction = 'down';
 
 var xPos = document.getElementById('X')
 var yPos = document.getElementById('Y')
+var Dir = document.getElementById('dir')
+
+var offsetX = 0;
+var offsetY = 0;
 
 function reset(){
     stop();
     x = 0;
     y = 0;
+    offsetX = 0;
+    offsetY = 0;
     clearInterval(child)
+    direction = 'down';
     child1.style.top =  x + 'px';
     child1.style.left =  y + 'px';
     child2.style.bottom = x + 'px'
     child2.style.right = y + 'px'
     startBtn.classList.remove('disable')
-    xPos.innerHTML = `x = ${x}`
-    yPos.innerHTML = `x = ${y}`
-    direction = 'down';
+    // xPos.innerHTML = `x = ${x}`
+    // yPos.innerHTML = `x = ${y}`
+    // Dir.innerHTML = `Dir = ${direction}`
+    
 }
 
 function stop(){
@@ -37,22 +45,16 @@ function stop(){
     startBtn.classList.remove('disable')
 }
 
-
 function start(){
     child = setInterval(moveBox, 10);
     moveBox;
     startBtn.classList.add('disable')
 }
 
-var offsetX = 0;
-var offsetY = 0;
-// var xPos = child1
 function moveBox(){
     if(direction == 'down'){
         if(x <= 418 - offsetY){
             x += speed;
-            // child1.style.top =  x + 'px';
-            // child1.style.left = y + 'px';
         }
         else{
             direction = 'right';
@@ -61,8 +63,6 @@ function moveBox(){
     else if(direction == 'right'){
         if(y <= 418 - offsetX){
             y += speed;
-        //     child1.style.top = x + 'px'
-        //     child1.style.left = y + 'px'
         }
         else{
             direction = 'up';
@@ -71,8 +71,6 @@ function moveBox(){
     else if(direction == 'up'){
         if(x >= offsetY){
             x -= speed;
-            // child1.style.top = x + 'px'
-            // child1.style.left = y + 'px'
         }
         else{
             direction = 'left';
@@ -94,7 +92,8 @@ function moveBox(){
     child2.style.bottom = x + 'px'
     child2.style.right = y + 'px'
 
-    xPos.innerHTML = `x = ${x}`
-    yPos.innerHTML = `x = ${y}`
+    // xPos.innerHTML = `x = ${x}`
+    // yPos.innerHTML = `x = ${y}`
+    // Dir.innerHTML = `Dir = ${direction}`
 
 }
